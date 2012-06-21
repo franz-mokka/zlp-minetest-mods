5108807dcfe012faa6251007ebd5e47f9c300a14
Franz Zapata <franz.zpt@gmail.com>
2012-06-20 18:57:33 -0600

    Player list command

--- commands/init.lua
+++ commands/init.lua
@@ -3,6 +3,7 @@
 --------------------------------------------------------------------------------
 --	Many custom commands, currently includes:
 --		/killme		Suicide
+--		/playerlist	List players
 --
 --	(c)2012 Fernando Zapata
 --	Code licensed under GNU GPLv2
@@ -17,6 +18,19 @@ minetest.register_chatcommand( 'killme', {
 	func = function( name, param )
 		local player = minetest.env:get_player_by_name(name)
 		player:set_hp(0)
+	end
+})
+
+---- List connected players ----
+minetest.register_chatcommand( 'playerlist', {
+	descrpition = 'list connected players',
+	func = function( name, param )
+		local players = minetest.get_connected_players()
+		local list = ''
+		for i,j in ipairs(players) do
+			list = list .. j:get_player_name() .. ' '
+		end
+		minetest.chat_send_player( name, list )
 	end
 })
 
